6d64b1afea905feb90637684364a779ba285b728
charlycop <charlycop@free.fr>
2024-05-22 11:57:10 +0200

    added nllb 600 distilled for CPU

---
+++ scripts/facebook_nllb-200-distilled-600M/README.md
@@ -0,0 +1,8 @@
+Link : https://huggingface.co/facebook/nllb-200-distilled-600M
+
+### To install : 
+- `cd ../../Models;git clone https://huggingface.co/facebook/nllb-200-distilled-600M;cd ../scripts/facebook_nllb-200-distilled-600M`
+
+### To Run :
+- `python3 run_nllb-200-distilled-600M_cpu.py` (for cpu version)
+- `python3 run_nllb-200-distilled-600M_gpu.py` (for gpu version needs to be done)
---
+++ scripts/facebook_nllb-200-distilled-600M/run_nllb-200-distilled-600M_cpu.py
@@ -0,0 +1,19 @@
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, pipeline
+
+model_location = "../../Models/nllb-200-distilled-600M"
+# Load the model and tokenizer
+tokenizer = AutoTokenizer.from_pretrained(model_location)
+model = AutoModelForSeq2SeqLM.from_pretrained(model_location)
+
+model = pipeline('translation', tokenizer=tokenizer, src_lang='eng_Latn', tgt_lang='fra_Latn', max_length = 200, model=model)
+
+text="My name is Charly."
+
+# Generate text with the model
+output = model(text, max_length=50, do_sample=True, top_k=50, top_p=0.95, num_return_sequences=1)[0]['translation_text']
+
+# Print the output
+print(f"Input: {text}")
+print(f"Output: {output}")
+
+
