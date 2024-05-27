3767e9d0b6b305b18681f88697a335a9c1ace6c3
charlycop <charlycop@free.fr>
2024-05-27 15:57:11 +0200

    Added another-repo as a submodule

---
+++ scripts/Benchmark/sacreBleu/basic_implementation.py
@@ -0,0 +1,11 @@
+from sacrebleu.metrics import BLEU, CHRF, TER
+
+refs = [['The dog bit the man.', 'It was not unexpected.', 'The man bit him first.'],
+        ['The dog had bit the man.', 'No one was surprised.', 'The man had bitten the dog.']]
+sys = ['The dog bit the man.', "It wasn't surprising.", 'The man had just bitten him.']
+
+bleu = BLEU()
+bleu.corpus_score(sys, refs)
+bleu.get_signature()
+chrf = CHRF()
+chrf.corpus_score(sys, refs)
